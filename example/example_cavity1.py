16ffbbb768956d7a9171280488ecaeec6bdad88c
niuliangtao <niuliangtao@qq.com>
2021-02-07 20:53:56 +0800

    add

--- example/example_cavity1.py
+++ example/example_cavity1.py
@@ -1,49 +1,36 @@
-
-import math
 import time
 
-from .lattice import *
+from notelbm.lattice import *
 
 ###############################################
 # LBM lid-driven cavity
 ###############################################
 
 # Free parameters
-Re_lbm = 100.0
+Re_lbm = 1000.0
 u_lbm = 0.03
-L_lbm = 400
-t_max = 8.0
+L_lbm = 600
+t_max = 20.0
 
 # Deduce other parameters
-Cs = 1.0/math.sqrt(3.0)
+Cs = 1.0 / math.sqrt(3.0)
 nx = L_lbm
-nu_lbm = u_lbm*L_lbm/Re_lbm
-tau_lbm = 0.5 + nu_lbm/(Cs**2)
+nu_lbm = u_lbm * L_lbm / Re_lbm
+tau_lbm = 0.5 + nu_lbm / (Cs ** 2)
 rho_lbm = 1.0
-dt = Re_lbm*nu_lbm/L_lbm**2
-it_max = math.floor(t_max/dt)
+dt = Re_lbm * nu_lbm / L_lbm ** 2
+it_max = math.floor(t_max / dt)
 
 # Other parameters
-output_freq = 2000
+output_freq = 500
 dpi = 200
 
 # Initialize lattice
-lattice = Lattice(nx=nx,
-                  ny=nx,
-                  dx=1.0/nx,
-                  dt=dt,
-                  tau_lbm=tau_lbm,
-                  Re_lbm=Re_lbm,
-                  u_lbm=u_lbm,
-                  L_lbm=L_lbm,
-                  nu_lbm=nu_lbm,
-                  rho_lbm=rho_lbm,
-                  dpi=dpi,
-                  t_max=t_max,
-                  it_max=it_max)
+lattice = Lattice(nx=nx, ny=nx, dx=1.0 / nx, dt=dt, tau_lbm=tau_lbm, Re_lbm=Re_lbm, u_lbm=u_lbm, L_lbm=L_lbm,
+                  nu_lbm=nu_lbm, rho_lbm=rho_lbm, dpi=dpi, t_max=t_max, it_max=it_max)
 
 # Initialize fields
-lattice.set_cavity(-u_lbm, -2.0*u_lbm, -1.0*u_lbm, 1.5*u_lbm)
+lattice.set_cavity(u_lbm)
 lattice.rho *= rho_lbm
 
 # Set initial distributions
@@ -55,8 +42,7 @@ start_time = time.time()
 
 # Solve
 print('### Solving')
-while (lattice.compute):
-
+while lattice.compute:
     # Printings
     lattice.it_printings()
 
@@ -64,8 +50,7 @@ while (lattice.compute):
     lattice.macro()
 
     # Output field
-    lattice.output_fields(lattice.it, output_freq,
-                          u_norm=True, u_ctr=False, u_stream=False)
+    lattice.output_fields(lattice.it, output_freq, u_norm=True, u_stream=False)
 
     # Compute equilibrium state
     lattice.equilibrium()
