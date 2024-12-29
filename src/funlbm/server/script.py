befec77a58efcb5c6880ac679365fa2bed2068fd
farfun <1007530194@qq.com>
2024-12-29 22:03:17 +0800

    add

--- src/funlbm/server/script.py
+++ src/funlbm/server/script.py
@@ -6,12 +6,13 @@ from funlbm.lbm import LBMD3, Config
 document_url = "https://darkchat.yuque.com/org-wiki-darkchat-gfaase/ul41go"
 
 
-def work(config="./config.json"):
-    if not os.path.exists(config):
+def work(args):
+    config_file = args.config
+    if not os.path.exists(config_file):
         info = f"""配置文件不存在，访问{document_url}去配置参数吧"""
         print(info)
-        raise FileExistsError(config)
-    _config = Config().from_file(config)
+        raise FileExistsError(config_file)
+    _config = Config().from_file(config_file)
     lbm = LBMD3(config=_config, device=_config.device)
     lbm.run()
 
