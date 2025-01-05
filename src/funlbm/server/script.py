52e62eef05c1d329f7d056737224e14c0454e2cc
farfun <1007530194@qq.com>
2025-01-05 15:58:50 +0800

    add

--- src/funlbm/server/script.py
+++ src/funlbm/server/script.py
@@ -1,35 +1,23 @@
-import argparse
 import os
 
+import click
 from funlbm.lbm import LBMD3, Config
 
 document_url = "https://darkchat.yuque.com/org-wiki-darkchat-gfaase/ul41go"
 
 
-def work(args):
-    config_file = args.config
-    if not os.path.exists(config_file):
+@click.group()
+def funlbm():
+    pass
+
+
+@funlbm.command()
+@click.option("--config", default="./config.json", help=f"参数配置，{document_url}")
+def run(config: str = "./config.json"):
+    if not os.path.exists(config):
         info = f"""配置文件不存在，访问{document_url}去配置参数吧"""
         print(info)
-        raise FileExistsError(config_file)
-    _config = Config().from_file(config_file)
+        raise FileExistsError(config)
+    _config = Config().from_file(config)
     lbm = LBMD3(config=_config, device=_config.device)
     lbm.run()
-
-
-def funlbm():
-    parser = argparse.ArgumentParser(prog="PROG")
-
-    # 添加子命令
-
-    parser.add_argument(
-        "--config",
-        default="./config.json",
-        type=str,
-        help=f"配置文件，参考{document_url}",
-    )
-
-    parser.set_defaults(func=work)
-
-    args, unknown = parser.parse_known_args()
-    args.func(args)
