c42f024551398165fd26b7ba3cbbc48bf0ad49f7
farfun <1007530194@qq.com>
2025-02-05 19:20:54 +0800

    add

--- example/freefall/main.py
+++ example/freefall/main.py
@@ -1,6 +1,6 @@
 from funlbm.lbm import LBMD3, Config
 
-lbm = LBMD3(config=Config().from_file("./config.json"), device="cpu")
+lbm = LBMD3(config=Config().from_file("./config.json"))
 lbm.run()
 
 # pip install funlbm -U -i  https://pypi.org/simple
