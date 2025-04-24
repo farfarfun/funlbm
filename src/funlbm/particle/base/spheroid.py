fb650489d27006920cacac8a57047233bd6537c7
farfun <1007530194@qq.com>
2025-04-24 21:42:19 +0800

    fix: 优化Spheroid类计算逻辑并更新版本
    - 修正Spheroid类中ra和rb参数的默认值设置
    - 重构compute_vector方法中的角度计算逻辑
    - 简化表面速度计算公式
    - 更新项目版本至1.2.18
    
    此次修改主要解决了Spheroid类中参数处理不完善的问题，优化了向量计算逻辑，同时进行了常规版本更新。

--- src/funlbm/particle/base/spheroid.py
+++ src/funlbm/particle/base/spheroid.py
@@ -6,11 +6,15 @@ from .ellipsoid import Ellipsoid
 
 
 class Spheroid(Ellipsoid):
+    """
+    https://darkchat.yuque.com/org-wiki-darkchat-gfaase/uvmi28/wlbp0rf6bck1ppfm
+    """
+
     def __init__(self, ra=None, rb=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.ra = ra or self.config.get("a") or 20
-        self.rb = rb or self.config.get("b") or 10
-        self.rc = rb or self.config.get("b") or 10
+        self.ra = ra or self.config.get("a") or 10
+        self.rb = ra or self.config.get("a") or 10
+        self.rc = rb or self.config.get("b") or 20
 
     def compute_vector(self) -> np.array:
         self.update()
@@ -22,45 +26,21 @@ class Spheroid(Ellipsoid):
         if B1 is None or B1 == 0:
             return np.zeros_like(lx)
 
-        # numerator = (x / self.ra) - 1
-        # denominator = (x**2 / self.ra**4) + (y**2 / self.rb**4) + (z**2 / self.rc**4)
-        # scale = numerator / denominator
-        # angle = np.array([self.ra - x, -y, -z]) - scale * np.array(    [x / self.ra**2, y / self.rb**2, z / self.rc**2] )
-        angle = np.array([(self.ra**2 - x**2) / -np.abs(x), -y, -z])
+        angle = np.array([-x, -y, (self.rc**2 - z**2) / -np.abs(z)])
 
         """
         将笛卡尔坐标 (x, y, z) 转换为长球体坐标 (tau, xi, phi).
         """
-        c = np.sqrt(abs(self.ra**2 - self.rb**2)) + 0.00000000000001
-        r1 = np.sqrt(x**2 + y**2 + (z + c) ** 2)
-        r2 = np.sqrt(x**2 + y**2 + (z - c) ** 2)
-        tau = (r1 + r2) / (2 * c)
-        xi = z / (c * tau)
-        phi = np.arctan2(y, x)
+
+        xi = np.arccos(z / np.sqrt(x**2 + y**2 + z**2 - self.ra**2))
 
         """
         计算椭球 Squirmer 的表面速度 u_s.
         """
-
-        # 极角方向的单位向量 e_xi
-        e_xi = np.array(
-            [
-                -xi * np.sqrt(1 - xi**2) * np.cos(phi),
-                -xi * np.sqrt(1 - xi**2) * np.sin(phi),
-                np.sqrt(1 - xi**2),
-            ]
-        )
-        tau0 = self.ra / (np.sqrt(self.ra**2 - self.rb**2))
+        tau0 = self.rc / (np.sqrt(self.rc**2 - self.ra**2))
 
         # 表面速度公式
-        size = (
-            -B1
-            * tau0
-            * np.sqrt(1 - xi**2)
-            * np.sqrt(tau0**2 - xi**2)
-            * (1 + beta * xi)
-            * e_xi
-        )
+        size = -B1 * tau0 * np.sqrt(1 - xi**2) * np.sqrt(tau0**2 - xi**2) * (1 + beta * xi)
         U0 = B1 * tau0 * (tau0 - (tau0**2 - 1) * 0.5 * np.log((tau0 + 1) / (tau0 - 1)))
         logger.warning(f"理想游动速度为:{U0}")
         return np.transpose(angle / np.linalg.norm(angle, axis=0) * size)
