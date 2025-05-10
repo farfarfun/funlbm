01bbb08b473a9c6c77f88a0febdb66903fd12a70
“bingtao” <farfarfun@qq.com>
2025-05-10 17:36:08 +0800

    add

--- example/freefall/main.py
+++ example/freefall/main.py
@@ -1,15 +1,7 @@
 import os.path
 
-from funlbm.file.tecplot import convert_particle
-from funlbm.lbm import create_lbm, create_lbm_by_checkpoint
+from funlbm.lbm import LBMD3
 
-# pip install funlbm -U -i  https://pypi.org/simple
-if os.path.exists("./data2") and True:
-    lbm = create_lbm_by_checkpoint("./data")
-else:
-    lbm = create_lbm()
-
+lbm=LBMD3()
 
 lbm.run(max_steps=100000)
-
-convert_particle(lbm.particle_swarm.particles[0])
